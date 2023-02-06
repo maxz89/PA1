0e8c44e7c62e03a7312d921d42bbfc5b6ad8b4be
maxz89 <max.zhao89@gmail.com>
2023-02-06 17:05:54 -0500

    set up server-client connection for only client at a time only

--- client.py
+++ client.py
@@ -1,9 +1,35 @@
 import socket
 import threading
 import sys 
+import argparse
+from socket import *
 
 
 #TODO: Implement a client that connects to your server to chat with other clients here
+
+# parsing command line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('-join', action='store_true')
+parser.add_argument('-host')
+parser.add_argument('-port')
+parser.add_argument('-passcode')
+args = parser.parse_args()
+join, server_ip, server_port, PASSCODE = args.join, args.host, args.port, args.passcode
+
+# intializing client socket and establishing connection
+client_socket = socket(AF_INET, SOCK_STREAM)
+client_socket.connect((server_ip, int(server_port)))
+client_socket.send((PASSCODE+"1").encode())
+connection_status = client_socket.recv(1024).decode()
+if connection_status == "0":
+	print("Incorrect passcode.")
+else:
+	connected = 1
+	while(connected):
+		# placeholder
+		i = 0
+
+
 
 # Use sys.stdout.flush() after print statemtents
 
--- server.py
+++ server.py
@@ -1,9 +1,38 @@
 import socket
 import threading
 import sys 
-
+import argparse
+from socket import *
 
 #TODO: Implement all code for your server here
+
+# parsing command line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('-start', action='store_true')
+parser.add_argument('-port')
+parser.add_argument('-passcode')
+args = parser.parse_args()
+start, server_port, PASSCODE = args.start, args.port, args.passcode
+
+# initializing server socket
+server_socket = socket(AF_INET, SOCK_STREAM)
+server_socket.bind(('', int(server_port)))
+server_socket.listen(1)
+print("Server started on port " + server_port + ". Accepting connections")
+sys.stdout.flush()
+
+# running server
+while(True):
+	connection_socket, addr = server_socket.accept()
+	message_with_flag = connection_socket.recv(1024).decode()
+	message = message_with_flag[:-1]
+	flag = message_with_flag[-1:]
+	if flag:
+		if message != PASSCODE:
+			connection_socket.send("0".encode())
+			connection_socket.close()
+
+	
 
 # Use sys.stdout.flush() after print statemtents
 
